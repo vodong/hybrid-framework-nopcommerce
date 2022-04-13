89aedbdf1057042ad2326fd8135a6d11c96c6db3
Pham Vo Dong <phamvodong@gmail.com>
2022-04-13 15:01:40 +0700

    Init automation framework

---
+++ testdata/com/nopcommerce/data/AdminData.java
@@ -0,0 +1,20 @@
+package com.nopcommerce.data;
+
+public class AdminData {
+	public class Register{
+		
+	}
+	
+	public class Login{
+		
+	}
+	
+	public class Address{
+		
+	}
+	
+	public class MyAccount{
+		
+	}
+
+}
---
+++ testdata/com/nopcommerce/data/ProductData.java
@@ -0,0 +1,20 @@
+package com.nopcommerce.data;
+
+public class ProductData {
+	public class Register{
+		
+	}
+	
+	public class Login{
+		
+	}
+	
+	public class Address{
+		
+	}
+	
+	public class MyAccount{
+		
+	}
+
+}
