2150c928f151a5bf3638f61e41170da7788cf1aa
Pham Vo Dong <phamvodong@gmail.com>
2022-05-23 20:33:58 +0700

    Create_Topic_Assert_Verify

---
+++ testcases/com/nopcommerce/user/User_13_Assert_Verify.java
@@ -0,0 +1,82 @@
+package com.nopcommerce.user;
+
+import static org.testng.Assert.assertEquals;
+
+import java.util.Random;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import commons.BaseTest;
+import commons.PageGeneratorManager;
+import pageObjects.nopcommerce.User.UserAddressPageObject;
+import pageObjects.nopcommerce.User.UserCustomerInfoPageObject;
+import pageObjects.nopcommerce.User.UserHomePageObject;
+import pageObjects.nopcommerce.User.UserLoginPageObject;
+import pageObjects.nopcommerce.User.UserMyProductReviewPageObject;
+import pageObjects.nopcommerce.User.UserRegisterPageObject;
+import pageObjects.nopcommerce.User.UserRewardPointPageObject;
+
+public class User_13_Assert_Verify extends BaseTest {
+	private WebDriver driver;
+	private String emailaddress,firstname,lastname,password,confirmpassword;
+	private UserHomePageObject homePage;
+	private UserRegisterPageObject registerPage;
+	private UserLoginPageObject loginPage;
+	private UserCustomerInfoPageObject customerInfor;
+
+  @Parameters("browser")
+  @BeforeClass
+  public void beforeClass(String browserName) {
+	  driver = getBrowserDriver(browserName);
+	  homePage = PageGeneratorManager.getHomePage(driver);
+
+	  emailaddress = "frameworkdpv_" + generateNumber() + "@yopmail.com";
+	  System.out.println(emailaddress);
+	  firstname = "A";
+	  lastname = "T";
+	  password = "123456";
+	  confirmpassword = "123456";
+  }
+
+  @Test
+  public void TC_01_Register_Login() { 
+	  registerPage = homePage.clickToRegisterLink();
+	  
+	  registerPage.inputToFirstNameTextBox(firstname);
+	  registerPage.inputToLastNameTextBox(lastname);
+	  registerPage.inputToEmailTextBox(emailaddress);
+	  registerPage.inputToPasswordTextBox(password);
+	  registerPage.inputToConfirmPasswordTextBox(confirmpassword);
+	  
+	  registerPage.clickToRegisterButton();
+	 
+	  verifyEquals(registerPage.getEmailSuccessMessage(), "Your registration completed...");
+	  
+	  registerPage.clickToLogoutButton();
+	  loginPage = homePage.clickToLoginLink();
+	  
+	  loginPage.inputToEmailTextBox(emailaddress);
+	  loginPage.inputToPasswordTextBox(password);
+	  
+	  homePage = loginPage.clickToLoginButton();
+	  verifyFalse(homePage.isMyAccountLinkDisplay()); //verifyTrue
+	  
+	  customerInfor = homePage.clickMyAccountLink();
+	  verifyFalse(customerInfor.isMyAccountPageDisplay()); //verifyTrue
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  driver.quit();
+  }
+  
+  public int generateNumber() {
+	  Random number = new Random();
+	  return number.nextInt(9999);
+  }
+
+}
