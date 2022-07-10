3fe362b4310f58bc0106d0286ad2c31c5b43db1d
phamvodong <phamvodong@gmail.com>
2022-07-10 13:48:16 +0700

    update

--- testcases/com/nopcommerce/user/User_19_Run_Browserstack.java
+++ testcases/com/nopcommerce/user/User_19_Run_Browserstack.java
@@ -1,7 +1,6 @@
 package com.nopcommerce.user;
 
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 import java.lang.reflect.Method;
